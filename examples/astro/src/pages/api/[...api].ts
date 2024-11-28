4693be5615f145f2e2875ec0618b91a0364792dc
dswbx <dennis.senn@gmx.ch>
2024-11-28 11:22:02 +0100

    added db for astro

--- examples/astro/src/pages/api/[...api].ts
+++ examples/astro/src/pages/api/[...api].ts
@@ -3,16 +3,19 @@ import { App } from "bknd";
 
 export const prerender = false;
 
+let app: App;
 export const ALL: APIRoute = async ({ request }) => {
-   const app = App.create({
-      connection: {
-         type: "libsql",
-         config: {
-            url: "http://127.0.0.1:8080"
+   if (!app) {
+      app = App.create({
+         connection: {
+            type: "libsql",
+            config: {
+               url: "http://127.0.0.1:8080"
+            }
          }
-      }
-   });
+      });
 
-   await app.build();
+      await app.build();
+   }
    return app.fetch(request);
 };
