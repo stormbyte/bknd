00f52eb0969dc3cc29e96c723639a5aebc00280e
dswbx <dennis.senn@gmx.ch>
2025-03-03 07:10:32 +0100

    minor fixes for nextjs

--- examples/nextjs/src/app/api/[[...bknd]]/route.ts
+++ examples/nextjs/src/app/api/[[...bknd]]/route.ts
@@ -1,5 +1,9 @@
 import { getApp } from "@/bknd";
 
+// if you're not using a local media adapter, or file database,
+// you can uncomment this line to enable running bknd on edge
+// export const runtime = "edge";
+
 const handler = async (request: Request) => {
    const app = await getApp();
    return app.fetch(request);
--- examples/nextjs/src/bknd.ts
+++ examples/nextjs/src/bknd.ts
@@ -2,12 +2,20 @@ import { type NextjsBkndConfig, getApp as getBkndApp } from "bknd/adapter/nextjs
 import { registerLocalMediaAdapter } from "bknd/adapter/node";
 import { headers } from "next/headers";
 
+// The local media adapter works well in development, and server based
+// deployments. However, on vercel or any other serverless deployments,
+// you shouldn't use a filesystem based media adapter.
+//
+// Additionally, if you run the bknd api on the "edge" runtime,
+// this would not work as well.
+//
+// For production, it is recommended to uncomment the line below.
 registerLocalMediaAdapter();
 
 export const config = {
    connection: {
-      // make sure to use a remote URL for production!
-      url: "file:data.db",
+      url: process.env.DB_URL as string,
+      authToken: process.env.DB_TOKEN as string,
    },
 } as const satisfies NextjsBkndConfig;
 
