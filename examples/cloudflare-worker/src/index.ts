12c955155d266dbe5536e21fa663a609d14a14ba
dswbx <dennis.senn@gmx.ch>
2025-06-09 20:27:02 +0200

    bump rc

--- examples/cloudflare-worker/src/index.ts
+++ examples/cloudflare-worker/src/index.ts
@@ -1,15 +1,8 @@
-import { type D1Connection, serve } from "bknd/adapter/cloudflare";
+import { serve } from "bknd/adapter/cloudflare";
 
 export default serve({
    mode: "warm",
    d1: {
       session: true,
-   },
-   onBuilt: async (app) => {
-      app.modules.server.get("/custom", async (c) => {
-         const conn = c.var.app.em.connection as D1Connection;
-         const res = await conn.client.prepare("select * from __bknd limit 1").all();
-         return c.json({ hello: "world", res });
-      });
    },
 });
