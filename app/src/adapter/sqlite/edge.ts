e6ee75c712648f124715d049616fcf053de85552
dswbx <dennis.senn@gmx.ch>
2025-07-02 16:08:33 +0200

    update edge sqlite import (libsql)

--- app/src/adapter/sqlite/edge.ts
+++ app/src/adapter/sqlite/edge.ts
@@ -1,5 +1,4 @@
-import type { Connection } from "bknd/data";
-import { libsql } from "../../data/connection/sqlite/libsql/LibsqlConnection";
+import { type Connection, libsql } from "bknd/data";
 
 export function sqlite(config: { url: string }): Connection {
    return libsql(config);
